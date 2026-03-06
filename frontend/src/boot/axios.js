2b824607bfe9101bb9f30a0ea53b5346dbe77e42
KinPa01 <autodonut.2548@gmail.com>
2026-03-06 14:26:16 +0700

    3

--- frontend/src/boot/axios.js
+++ frontend/src/boot/axios.js
@@ -1,8 +1,10 @@
 import { defineBoot } from '#q-app/wrappers'
 import axios from 'axios'
 
-// สร้างเพียงครั้งเดียว และใช้ baseURL เป็น /api เท่านั้น
-const api = axios.create({ baseURL: '/api' })
+// เปลี่ยนจาก /api เป็น URL ของ Render ที่คุณเพิ่งได้มา
+const api = axios.create({
+  baseURL: 'https://kinpa01-pakin-intalang-365-06-04-2026-2.onrender.com/api'
+})
 
 export default defineBoot(({ app }) => {
   app.config.globalProperties.$axios = axios
